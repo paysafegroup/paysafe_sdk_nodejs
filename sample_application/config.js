ee472791164a82f880626515189d01c8fac43d4e
Asawari Vaidya <asawari.vaidya@opusconsulting.com>
2016-10-20 18:23:18 +0530

    sdk with sample applications

--- sample_application/config.js
+++ sample_application/config.js
@@ -3,11 +3,11 @@ var paysafeApiKeySecret;
 var paysafeAccountNumber;
 var paysafeEnvironment;
 
-exports.paysafeApiKeyId = 'devcentre4651';
-exports.paysafeApiKeySecret = 'B-qa2-0-54b5374e-302d02147911306b5c6db4ad74e083803733163195c75ef902150095b4a21a5ea7f4c19b7b37aed1944490b59e785e';
-exports.paysafeAccountNumber = '1000032987';
+exports.paysafeApiKeyId = 'your-api-key';
+exports.paysafeApiKeySecret = 'your-api-key-secret';
+exports.paysafeAccountNumber = 'your-account-number';
 exports.paysafeEnvironment = 'TEST';// TEST or LIVE
 
 
-exports.currency_code = 'USD';
+exports.currency_code = 'currecy-code';
 exports.currency_base_units_multiplier = 100;
